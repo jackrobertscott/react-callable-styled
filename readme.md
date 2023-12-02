abc8acc0413dc24bd45fcbffb099e184dabed296
Jack Scott <jack.rob.scott@gmail.com>
2023-12-02 10:50:51 +0800

    added code and readme docs

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # react-callable-styled
 
-> React callable components with CSS styling.
+> React callable components with CSS styling using `@emotion/css`.
 
 ## Install
 
@@ -10,17 +10,49 @@ npm install react-callable-styled
 
 ## Usage
 
+The `react-callable-styled` library allows you to create styled components in React using a syntax similar to styled-components. Here's a basic usage example:
+
 ```ts
-// todo
+import { styled } from 'react-callable-styled';
+
+const MyStyledComponent = styled.div`
+  background-color: papayawhip;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+// Use MyStyledComponent like a regular React component
 ```
 
 ## API
 
-Todo...
+### `styled`
+
+A Proxy object allowing you to create styled components for each HTML element.
+
+#### Usage:
+
+```ts
+const StyledTag = styled.tagName`...styles`;
+```
+
+- **tagName**: string - Any valid HTML tag name.
+- **styles**: TemplateStringsArray - CSS styles.
+
+### `StyledComponentArgs`
+
+Type definition for the arguments accepted by a styled component. It can either be a TemplateStringsArray with any additional values, or an array of CSSInterpolation.
+
+### `StyledProxy`
+
+Defines a type mapping each key in `HTMLElementTagNameMap` to a function accepting `StyledComponentArgs`.
 
 ## Contributing
 
-Contributions are always welcome!
+Contributions are always welcome! Feel free to open a pull request or an issue to propose changes or add new features.
 
 ## License
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,52 @@
-console.log("Hello, World!")
+import { CSSInterpolation, css } from "@emotion/css"
+import { createElement } from "react"
+import { ComponentCb } from "react-callable-components"
+
+/**
+ * Type definition for the arguments accepted by StyledComponent.
+ * It can either be a TemplateStringsArray with any additional values,
+ * or an array of CSSInterpolation.
+ */
+export type StyledComponentArgs =
+  | [TemplateStringsArray, ...any[]]
+  | CSSInterpolation[]
+
+/**
+ * Defines a type for the StyledProxy. This type maps each key in HTMLElementTagNameMap
+ * to a function that takes StyledComponentArgs and returns a ComponentCb with
+ * `className` and `dotClassName` properties.
+ */
+export type StyledProxy = {
+  [K in keyof HTMLElementTagNameMap]: (
+    ...args: StyledComponentArgs
+  ) => ComponentCb<K> & { className: string; dotClassName: string }
+}
+
+/**
+ * Creates a new styled proxy. This proxy handles creating styled components
+ * for each HTML element. It uses a get trap to intercept property access
+ * and returns a component builder function for the requested HTML element.
+ */
+export const styled = new Proxy<StyledProxy>({} as any, {
+  /**
+   * The get trap for the proxy. It is invoked when accessing a property on the styled object.
+   * @param _ - The target object of the proxy. Not used in this implementation.
+   * @param property - The name of the HTML element to create a styled component for.
+   * @returns A function that creates a styled component for the specified HTML element.
+   */
+  get<T extends keyof HTMLElementTagNameMap>(_: any, property: T) {
+    const cb: StyledProxy[T] = (...args) => {
+      const elementClass = css(args)
+      function component(props: any) {
+        let cn = props.className
+        cn = Array.isArray(cn) ? cn : [cn]
+        props.className = [...cn, elementClass]
+        return createElement(property, props)
+      }
+      component.className = elementClass
+      component.dotClassName = "." + elementClass
+      return component
+    }
+    return cb
+  },
+})
